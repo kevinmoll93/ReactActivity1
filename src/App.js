c1a36d46f9fba58e5d0fcfc9893d3a4f3c3c1940
kevinmoll93 <kevinmollecundo@gmail.com>
2021-04-28 14:40:41 -0300

    Agrego Proyecto Completo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './assets/images/logo.svg';
+// import logo from './assets/images/logo.svg';
 import './assets/css/App.css';
+import { ToDoList } from './components/TodoList';
+import Badge from 'react-bootstrap/Badge'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="mt-5">
+        <h1><Badge variant="info">Puesto de Trabajo</Badge></h1>
+        <ToDoList />
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import List from "./List";
+import Form from "./Form";
+import Alert from 'react-bootstrap/Alert';
+
+export const ToDoList = () => {
+  const [todo, setTodo] = useState({
+    puesto: "",
+    empresa: "",
+    ciudad: "",
+    pais: "",
+  });
+  const [todos, setTodos] = useState([]);
+  const [error, setError] = useState(false);
+
+  const { puesto, empresa, ciudad, pais } = todo;
+
+  const updateState = (e) => {
+    setTodo({
+      ...todo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      puesto.trim() === "" ||
+      empresa.trim() === "" ||
+      ciudad.trim() === "" ||
+      pais.trim() === ""
+    ) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    setTodos([
+      ...todos,
+      {
+        id: uuidv4(),
+        puesto,
+        empresa,
+        ciudad,
+        pais,
+      },
+    ]);
+    setTodo({
+      puesto: "",
+      empresa: "",
+      ciudad: "",
+      pais: "",
+    });
+  };
+
+  const handleDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className="row">
+      <div className="col-6">
+        {error && (
+          <Alert variant="danger">
+          <Alert.Heading>Error</Alert.Heading>
+          <p>
+            Necesitas completar todos los campos
+          </p>
+        </Alert>
+        )}
+        <Form
+          handleSubmit={handleSubmit}
+          updateState={updateState}
+          puesto={puesto}
+          empresa={empresa}
+          ciudad={ciudad}
+          pais={pais}
+        />
+      </div>
+      <div className="col-6">
+        { todos.map((td) => ( 
+          <List td={td} handleDelete={handleDelete} />
+        ))}
+      </div>
+    </div>
+  );
+};
